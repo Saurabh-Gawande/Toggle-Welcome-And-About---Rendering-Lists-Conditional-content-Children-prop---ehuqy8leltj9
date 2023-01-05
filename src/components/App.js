a1a747da04580732335ce5c6458616c86b781fe6
Saurabh <saurabhgawande2016@gmail.com>
2023-01-05 23:08:40 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [change , setChange] = useState(true);
+  function Toggle(){
+    setChange(false)
+  }
   return (
     <div id="main">
-      <div id="welcome-div">
+      {change ? <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
-      </div>
+      </div> :
       <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      </div>}
+      <button id="toggle" onClick={Toggle}>Toggle</button>
     </div>
   );
 };
